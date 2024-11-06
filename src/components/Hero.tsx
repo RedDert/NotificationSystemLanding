ef487a23d7ee24614e300756749b7d4cb9487f99
DilanR <dilanredha@hotmail.com>
2024-11-06 01:29:51 +0100

    added arrow indicating user to scroll down

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,6 +11,23 @@ const Hero: React.FC = () => {
       >
         View on GitHub
       </a>
+      {/* Scroll Down Indicator */}
+      <div className="absolute bottom-10 flex flex-col items-center animate-bounce">
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19 9l-7 7-7-7"
+          ></path>
+        </svg>
+      </div>
     </section>
   );
 };
