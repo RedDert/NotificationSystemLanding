c7ccdc277ed41258038bc58a4fdd1a2c1065330d
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-11-06 15:47:52 +0100

    added project name to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from '@mui/material';
 const Footer = () => {
   return (
     <Box sx={{ py: 3, bgcolor: 'primary.main', color: 'text.secondary', textAlign: 'center' }}>
-      <Typography>&copy; {new Date().getFullYear()} Project Name. All rights reserved.</Typography>
+      <Typography>&copy; {new Date().getFullYear()} Nansi. All rights reserved.</Typography>
     </Box>
   );
 };
