d66af5a30ff8a6f247e815fd2ef616773725710d
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-11-07 20:24:47 +0100

    added footer to newTeams page

--- src/routes/new.tsx
+++ src/routes/new.tsx
@@ -1,10 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router';
 import TeamCreationForm from '../components/TeamCreationForm';
+import Footer from '../components/Footer';
 
 export const Route = createFileRoute('/new')({
   component: () => (
-    <div className="min-h-screen flex items-center justify-center">
-      <TeamCreationForm />
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow flex items-center justify-center">
+        <TeamCreationForm />
+      </div>
+      <Footer />
     </div>
   ),
 });
